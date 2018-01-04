51be7f2d7479c1dc769a1311db09070ba7412b85
uchiyamakoki <454768497@qq.com>
2018-01-04 15:05:34 +0800

    Delete MainThrowsDemo.java

--- src/Unit6/MainThrowsDemo.java
+++
@@ -1,8 +0,0 @@
-package Unit6;
-
-public class MainThrowsDemo {
-    public static void main(String[] args) throws Exception{
-        Math m=new Math();
-        System.out.println("除法操作："+m.div(5,0));
-    }
-}
